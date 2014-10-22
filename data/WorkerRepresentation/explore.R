e46387595c2e451122e5b4213bbc854b231c39c8
Alex Wang <alexwang@college.harvard.edu>
2014-10-21 18:31:56 -0700

    created bar graph using worker data, need to add to website

--- data/WorkerRepresentation/explore.R
+++ data/WorkerRepresentation/explore.R
@@ -2,17 +2,29 @@
 # explore.R
 # script for loading data and plotting one survey question from Worker Representation and Participation dataset on Dataverse
 
+library(rjson)
+
 # remove objects from current environment
 rm(list=ls())
 
 # set working directory
-setwd("/Users/vjdorazio/Desktop/IQSS/eyeData/study_GITCZNWZYE")
+setwd("Projects/eyeData/data")
 
 # read data
-mydata <- read.delim("data/wrps1.tab")
+mydata <- read.delim("WorkerRepresentation/Data/wrps1.tab")
 
 # table the data
-plottable <- table(mydata$q1)
+plottable <- table(mydata$s5)
+
+str(plottable)
 
 # plot a barplot
 barplot(plottable)
+
+test <- toJSON(plottable)
+
+sink("worker.json")
+cat(toJSON((plottable)))
+sink()
+
+file.show("worker.json")
