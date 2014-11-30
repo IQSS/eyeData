6720f19707e8f4569211c593977e45d3ec043f9c
Luis Perez <luisperez@college.harvard.edu>
2014-11-30 18:21:00 -0500

    Refactoring code

--- eyedata/static/js/eyedata.js
+++ eyedata/static/js/eyedata.js
@@ -3,50 +3,12 @@
  
 // On document load
 $(document).ready(function(){
-  $("#flip").click(function(){
-    $("#panel").slideToggle("slow");
+  // allow sorting of results on 
+  $("#datasets-table").tablesorter({
+    sortList: [[0,0], [1,0]]
   });
+
+  // make table scrollable
+  console.log("loaded");
 });
 
-// Create SVG element
-function visualize() { var svg = d3.select(".cover-container")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
-
-  svg.selectAll("rect")
-      .data(dataset)
-      .enter()
-      .append("rect")
-      .attr("x", function(d, i) {
-          return i * (w / dataset.length);
-      })
-      .attr("y", function(d) {
-          return h - 2.5*d;
-      })
-      .attr("width", w / dataset.length - padding)
-      .attr("height", function(d) {
-          return 2.5*d;
-      })
-      .attr("fill", function(d) {
-          return "rgb(0, 0, " + (d / 2.5) + ")";
-      });
-
- svg.selectAll("text")
-      .data(dataset)
-      .enter()
-      .append("text")
-      .text(function(d) {
-          return d;
-      })
-      .attr("x", function(d, i) {
-          return i * (w / dataset.length) + (w / dataset.length - padding) / 2;
-      })
-      .attr("y", function(d) {
-          return h - (2.5* d) + 14;
-      })
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "11px")
-      .attr("fill", "white")
-      .attr("text-anchor", "middle");
-}
