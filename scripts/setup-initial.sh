25d083f9c1a0a355483e1bf9af001518288f2a98
Philip Durbin <philip_durbin@harvard.edu>
2014-12-03 11:49:42 -0500

    put `yum install python27` back, removed in d992cb2

--- scripts/setup-initial.sh
+++ scripts/setup-initial.sh
@@ -15,6 +15,7 @@ yum install -y gcc-c++
 echo "Installing Python 2.7"
 rpm --import http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/os/RPM-GPG-KEY-sl
 yum install -y http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/external_products/softwarecollections/yum-conf-softwarecollections-1.0-1.el6.noarch.rpm
+yum install -y python27
 # for scipy
 yum install blas-devel lapack-devel
 echo "Setting up Django app with Python 2.7"
